0d67364e39cc9e4495e79d3aa1f011dd18e27e88
stephen.more@gmail.com <stephen.more@gmail.com@a6480d51-c4f1-15aa-67c3-cc9d53c9d136>
2012-01-16 12:50:52 +0000

    Throttling back the items sent to GAE.

--- trunk/mvn/plug-jamrData/src/main/java/com/googlecode/jamr/plug/JamrDataConfig.java
+++ trunk/mvn/plug-jamrData/src/main/java/com/googlecode/jamr/plug/JamrDataConfig.java
@@ -21,6 +21,8 @@ package com.googlecode.jamr.plug;
 
 public class JamrDataConfig {
 	private String accessCode;
+	private int itemsInQueue;
+	private int minutesBetween;
 
 	public String getAccessCode() {
 		return accessCode;
@@ -28,5 +30,21 @@ public class JamrDataConfig {
 
 	public void setAccessCode(String ac) {
 		this.accessCode = ac;
+	}
+
+	public int getItemsInQueue() {
+		return itemsInQueue;
+	}
+
+	public void setItemsInQueue(int i) {
+		this.itemsInQueue = i;
+	}
+
+	public int getMinutesBetween() {
+		return minutesBetween;
+	}
+
+	public void setMinutesBetween(int mb) {
+		this.minutesBetween = mb;
 	}
 }
