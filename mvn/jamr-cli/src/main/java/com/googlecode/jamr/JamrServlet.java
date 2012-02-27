698b32534e4a2282b71443e0dcd361edd86aabe9
stephen.more <stephen.more@gmail.com>
2012-02-27 01:13:55 +0000

    Can now output in json.

--- mvn/jamr-cli/src/main/java/com/googlecode/jamr/JamrServlet.java
+++ mvn/jamr-cli/src/main/java/com/googlecode/jamr/JamrServlet.java
@@ -4,8 +4,17 @@ public class JamrServlet extends javax.servlet.http.HttpServlet {
 	private static org.slf4j.Logger log = org.slf4j.LoggerFactory
 			.getLogger(JamrServlet.class);
 
+	private org.springframework.context.ApplicationContext context;
+	private org.apache.commons.dbcp.BasicDataSource ds;
+
 	public void init() throws javax.servlet.ServletException {
 		log.trace("Executing INIT");
+
+		com.googlecode.jamr.PlugUtils pu = new com.googlecode.jamr.PlugUtils();
+		context = new org.springframework.context.support.FileSystemXmlApplicationContext(
+				"file:" + pu.getConfigPath() + "jdbc");
+		ds = (org.apache.commons.dbcp.BasicDataSource) context
+				.getBean("h2DataSource");
 	}
 
 	public void service(javax.servlet.http.HttpServletRequest request,
@@ -15,40 +24,72 @@ public class JamrServlet extends javax.servlet.http.HttpServlet {
 		String operation = request.getPathInfo();
 		log.info("servicing: " + operation);
 
-		if (operation.equals("/dump")) {
-			com.googlecode.jamr.PlugUtils pu = new com.googlecode.jamr.PlugUtils();
-			org.springframework.context.ApplicationContext context = new org.springframework.context.support.FileSystemXmlApplicationContext(
-					"file:" + pu.getConfigPath() + "jdbc");
-			com.googlecode.jamr.plug.TargetList targetList = (com.googlecode.jamr.plug.TargetList) context
-					.getBean("TargetList");
-			java.util.List targets = targetList.getTargets();
-			for (int i = 0; i < targets.size(); i++) {
-				try {
+		try {
+			java.util.List output = new java.util.Vector();
+
+			java.sql.Connection conn = ds.getConnection();
+			if (operation.equals("/favicon.ico")) {
+			} else if (operation.equals("/dump")) {
+				String sql = "select serial, recorded_at, reading from meters";
+				log.info("SQL: " + sql);
+				java.sql.PreparedStatement ps = conn.prepareStatement(sql);
+				//ps.setString( 1, guid );
+				java.sql.ResultSet rs = ps.executeQuery();
+				while (rs.next()) {
+					java.util.Hashtable record = new java.util.Hashtable();
+					record.put("meter", rs.getString(1));
+					record.put("timestamp", rs.getDate(2).getTime() / 1000);
+					record.put("reading", rs.getString(3));
+					output.add(record);
+				}
+			} else if (operation.equals("/dumpTargets")) {
+				com.googlecode.jamr.plug.TargetList targetList = (com.googlecode.jamr.plug.TargetList) context
+						.getBean("TargetList");
+				java.util.List targets = targetList.getTargets();
+				for (int i = 0; i < targets.size(); i++) {
 					com.googlecode.jamr.plug.ErtmDAO dao = (com.googlecode.jamr.plug.ErtmDAO) targets
 							.get(i);
 					Object obj = dao.getDataSource();
-					String details = "";
 					if (obj instanceof org.apache.commons.dbcp.BasicDataSource) {
 						org.apache.commons.dbcp.BasicDataSource bds = (org.apache.commons.dbcp.BasicDataSource) obj;
-						details = bds.getDriverClassName();
+						output.add("JDBC: " + bds.getDriverClassName());
 					}
-					log.info("JDBC: " + details);
-				} catch (Exception e) {
-					java.io.StringWriter sw = new java.io.StringWriter();
-					java.io.PrintWriter pw = new java.io.PrintWriter(sw);
-					e.printStackTrace(pw);
-					log.error(sw.toString());
+				}
+			} else if (operation.equals("/meters")) {
+				String sql = "select distinct serial from meters order by serial";
+				log.info("SQL: " + sql);
+				java.sql.PreparedStatement ps = conn.prepareStatement(sql);
+				java.sql.ResultSet rs = ps.executeQuery();
+				while (rs.next()) {
+					output.add(rs.getString(1));
 				}
 			}
-		} else {
-			response.setContentType("text/html");
-			java.io.PrintWriter out = response.getWriter();
-			out.println("<pre>");
-			out.println("<h1>JAMR</h1>");
-			out.println("</pre>");
+
+			if (output.size() > 0) {
+				log.debug("Size of result set: " + output.size());
+				com.google.gson.GsonBuilder gsonBuild = new com.google.gson.GsonBuilder();
+				gsonBuild.setPrettyPrinting();
+				com.google.gson.Gson gson = gsonBuild.create();
+				response.setContentType("application/json");
+				String json = gson.toJson(output);
+				javax.servlet.ServletOutputStream sOutStream = response
+						.getOutputStream();
+				sOutStream.print(json);
+			} else {
+				response.setContentType("text/html");
+				java.io.PrintWriter out = response.getWriter();
+				out.println("<pre>");
+				out.println("<h1>JAMR</h1>");
+				out.println("</pre>");
+			}
+
+		} catch (Exception e) {
+			java.io.StringWriter sw = new java.io.StringWriter();
+			java.io.PrintWriter pw = new java.io.PrintWriter(sw);
+			e.printStackTrace(pw);
+			log.error(sw.toString());
 		}
 	}
-
 	public void destroy() {
 		log.trace("Executing DESTROY");
 	}
